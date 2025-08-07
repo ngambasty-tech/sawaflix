aeb6ce1bb2afad4ad2d74eb204ae28477d5f2297
asime domitila <asimedomitila@gmail.com>
2025-08-07 22:49:40 +0100

    the navbarbottom

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+
+//body here
+'use client';
+import NavbarBottom from "../components/common/NavbarBottom";
+export default function Home() {
+  return(
+    <>
+    <NavbarBottom/>
+    </>
+  )
+}
