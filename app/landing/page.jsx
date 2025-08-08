7a64d9e401b2df63a2d8bfd16981550144423f16
Ngsms22 <ngamsebastien@gmail.com>
2025-08-08 08:06:34 -0700

    landing page done

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,17 +1,41 @@
-// app/landing/page.jsx
-import MovieCarousel from "../../components/common/landingpagepart2";
-import LandingTop from "../../components/common/landingTop";
+'use client';
+import NavbarTop from "../../components/common/NavbarTop";
+import NavbarBottom from "../../components/common/NavbarBottom";
+import MoviesSection from "../../components/common/moviesForYou";
+import MusicSection from "../../components/common/MusicsForYou";
+import StreamVerseHeroBanner from "../../components/common/Hero"
+import TrendingSection from "../../components/common/Trending"
+import Image from 'next/image';
 
-export const metadata = {
-  title: 'Sawaflixx   The Ultimate Music And Movies',
-  description: 'Welcome to Sawaflixx   Your ultimate destination for music and movies',
-};
 
-export default function Landing() {
+export default function Home() {
+  const backgroundImage = "/bg-image.jpg";
+  const overlayOpacity = 0.4;
+  
   return (
-  <>
-  <LandingTop/>
-  <MovieCarousel/>
-  </>
-  )
+    <>
+      <div className="relative min-h-screen w-full overflow-x-hidden">
+        <div className="fixed inset-0 -z-20">
+          <Image
+            src={backgroundImage}
+            alt="Background"
+            fill
+            className="object-cover"
+            priority
+            quality={85}
+          />
+        </div>
+        
+        <div className="fixed inset-0 -z-10 bg-black" style={{ opacity: overlayOpacity }}></div>
+        <div className="fixed inset-0 -z-10 bg-gradient-to-b from-transparent via-black/20 to-black/60"></div>
+        
+        <NavbarTop />
+        <StreamVerseHeroBanner/>
+        <TrendingSection/>
+        <MoviesSection/>
+        <MusicSection/>
+        <NavbarBottom/>
+      </div>
+    </>
+  );
 }
