eeea133e3c7c6fddc18142a7b647d0d4ab4404e8
asime domitila <asimedomitila@gmail.com>
2025-08-11 20:40:17 +0100

    landing page

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,14 +1,15 @@
-'use client';
-// app/landing/page.jsx
-import LandingTop from "../../components/common/landingTop";
-import MovieCarousel from '../../components/common/landingpagepart2';
+// 'use client';
+// import LandingTop from "../../components/common/landingTop";
+// import MovieCarousel from '../../components/common/landingpagepart2';
+// import Image from 'next/image';
 
-
-export default function Landing() {
-  return (
-    <>
-      <LandingTop/>
-      <MovieCarousel />
-    </>
-  );
-}
+// export default function Landing() {
+//   return (
+  
+//       <div className="relative z-10">
+//         <LandingTop />
+//         <MovieCarousel />
+//       </div>
+    
+//   );
+// }
