1790d39254d7141fc1ee1308f5537662a0206137
asime domitila <asimedomitila@gmail.com>
2025-08-11 20:36:06 +0100

    initial

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,11 +1,14 @@
+'use client';
 // app/landing/page.jsx
 import LandingTop from "../../components/common/landingTop";
+import MovieCarousel from '../../components/common/landingpagepart2';
 
-export const metadata = {
-  title: 'Sawaflixx   The Ultimate Music And Movies',
-  description: 'Welcome to Sawaflixx   Your ultimate destination for music and movies',
-};
 
 export default function Landing() {
-  return <LandingTop/>;
+  return (
+    <>
+      <LandingTop/>
+      <MovieCarousel />
+    </>
+  );
 }
