9697bb39beac2a7750a2dec1be1010455a62b7c0
Ngsms22 <ngamsebastien@gmail.com>
2025-08-12 05:28:57 -0700

    day 5t

--- app/api/user/[id]/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET(request, {params}){
-    const param=await params
-    console.log('params: ', param.id)
-
-
-    // const id=await params.id
-    // console.log("id", id)
-    return Response.json({data:"my data arrived"})
-}
