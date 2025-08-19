8302d39e05b21f8a08f5fee697959532972b3911
Aarick <aarick47@gmail.com>
2025-08-19 14:12:43 +0100

    the rightsidebar of the page

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import RightSidebar from '../../../components/Dashboard/rightsidebar'
 
 const page = () => {
   return (
+    <>
     <div>
       Sawa home dashboard
     </div>
+    <RightSidebar/>
+    </>
   )
 }
 
