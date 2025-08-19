2beec2803635f547d7bae882562bd1c3ddc229f3
asime domitila <asimedomitila@gmail.com>
2025-08-19 14:15:26 +0100

    the leftsidebar of the page

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+// import LeftSidebar from '../../../components/Dashboard/leftsidebar';
+  
 const page = () => {
   return (
+    <>
     <div>
       Sawa home dashboard
     </div>
+    {/* <LeftSidebar/> */}
+    
+    
+    </>
   )
 }
 
