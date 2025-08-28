8e55d55639a0d09b5c4fdbe4177df973b3c3c5f9
Ngsms22 <ngamsebastien@gmail.com>
2025-08-28 09:35:51 +0100

    reels

--- components/Dashboard/SawaFlix.jsx
+++ components/Dashboard/SawaFlix.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react';
 import { Play, Star } from 'lucide-react';
+import Link from 'next/link';
 
 const SawaFlix = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -71,12 +72,16 @@ const SawaFlix = () => {
             The Ultimate Music And Movies Experience
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <Link href="/dashboard/contentReels">
             <button className="bg-red-600 hover:bg-red-700 text-white px-8 py-3 rounded-full font-semibold transition-colors duration-200 cursor-pointer">
               Watch Now
-            </button>
+            </button>  
+            </Link>
+            <Link href="/dashboard/reels">
             <button className="bg-transparent border-2 border-white text-white hover:bg-white hover:text-black px-8 py-3 rounded-full font-semibold transition-all duration-200 cursor-pointer">
               Listen Now
             </button>
+            </Link>
           </div>
         </div>
       </div>
