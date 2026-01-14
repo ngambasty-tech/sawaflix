0c25f0983a5a833d098dc36356c899a2aa9d0ea5
ngam <ngambasty@gmail.com>
2026-01-14 19:52:06 +0100

    fixed

--- app/api/youtube/comment/route.js
+++ app/api/youtube/comment/route.js
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from '../../../../utils/supabase/server';
 // import { getValidToken } from '@/utils/youtube/refresh-token';
 import { NextResponse } from 'next/server';
 
